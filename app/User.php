306a81193be019c53cc283b7b543e6deb4ba9212
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-06-22 01:26:02 +0800

    using mongodb and authenticated the user

---
+++ app/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class User extends Eloquent implements Authenticatable
+{
+    use AuthenticatableTrait;
+    use Notifiable;
+
+    protected $connection = 'mongodb';
+    
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
